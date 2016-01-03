f6de37440769bc2e892d3fb8eb4ca87863ea415e
Babalev <ivaylo.babalev@gmail.com>
2016-01-03 20:13:03 +0200

    adaptive design

---
+++ saitche/index.js
@@ -0,0 +1,8 @@
+﻿var output ='';
+var input =['soft','intelect','academy',2015];
+
+for (var i=input.length-1;i>=0; i++){
+	console.log ('dobavqm', input[i])
+	output = output+ '' +input[i];
+};
+console.log('output',output)
